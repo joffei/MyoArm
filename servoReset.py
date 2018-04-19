b6d60766df179cceeb548ca38c3eeb8be8323400
joffei <jeoffei@gmail.com>
2018-04-19 01:52:00 +0000

    improved execution of servoReset.py

--- servoReset.py
+++ servoReset.py
@@ -11,9 +11,12 @@ CENTER_PULSE = 1500
 
 pulse = 1010
 
+pi = pigpio.pi()
+print("initializing pi")
+
 while(pulse != 0):
     
-    pi = pigpio.pi()
+    
     
     pulse = (int(input("number between 1010 and 2000, 0 to exit: ")))
     
@@ -21,12 +24,13 @@ while(pulse != 0):
         print("too high")
         pulse = 1010
     elif(pulse == 0):
-        exit()
+        break
     elif(pulse < 1010):
         print("too low")
         pulse = 1010
     else:
         if (not pi.connected):
+            print("Pi not connected")
             exit()
         print("Init OK")
         print(pi.get_mode(SERVO_PIN))
@@ -37,3 +41,7 @@ while(pulse != 0):
         pi.set_servo_pulsewidth(SERVO_PIN, pulse)
         time.sleep(1)
         
+while (pi.connected):
+    pi.stop()
+    
+print("disconnected")
