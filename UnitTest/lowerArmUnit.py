8328a09bd6637a97db4fede72b96eb28dbd890b6
joffei <jeoffei@gmail.com>
2018-04-22 02:02:39 +0000

    moved 'lowerArmUnit' to unit test directory

--- UnitTest/lowerArmUnit.py
+++ UnitTest/lowerArmUnit.py
@@ -9,11 +9,9 @@ from servo import *
 
 armPi = pigpio.pi()
 
-cntPeriod = 0
-
 base = Servo(gpio=14, pulse=1500, minPulse=1000, maxPulse=1800, pwInc=15, interest=0, connected=True)
-lowerArm = Servo(gpio=15, pulse=1500, minPulse=1080, maxPulse=1800, pwInc=15, interest=1, connected=True)
-upperArm = Servo(gpio=18, pulse=1500, minPulse=1080, maxPulse=1800, pwInc=15, interest=2, connected=True)
+lowerArm = Servo(gpio=15, pulse=1500, minPulse=1080, maxPulse=1800, pwInc=20, interest=1, connected=True)
+upperArm = Servo(gpio=18, pulse=1500, minPulse=1080, maxPulse=1800, pwInc=20, interest=2, connected=True)
 wrist = Servo(gpio=18, pulse=1500, minPulse=1080, maxPulse=1800, pwInc=15, interest=3, connected=True)
 
    
@@ -29,45 +27,87 @@ def cb1(event, tick):
     global armPi
     
     while (SERVO[event].connected()):
-        SERVO[event].set_pulse(SERVO[event].get_pulse() + SERVO[event].get_pwInc())
+        if (SERVO[event].get_pulse() > SERVO[event].get_minPulse()+abs(SERVO[event].get_pwInc())) and (SERVO[event].get_pulse() < SERVO[event].get_maxPulse()-abs(SERVO[event].get_pwInc())):
+            SERVO[event].set_pulse(SERVO[event].get_pulse() + SERVO[event].get_pwInc())
         
+        #print(SERVO[event].get_pulse())
         armPi.set_servo_pulsewidth(SERVO[event].get_gpio(), SERVO[event].get_pulse())
-        time.sleep(0.03)
+        time.sleep(0.035)
 
 def onUnlock():
     armPi.set_servo_pulsewidth(base.get_gpio(), base.get_pulse())
     armPi.set_servo_pulsewidth(lowerArm.get_gpio(), lowerArm.get_pulse())
     armPi.set_servo_pulsewidth(upperArm.get_gpio(), upperArm.get_pulse())
     armPi.set_servo_pulsewidth(wrist.get_gpio(), wrist.get_pulse())
+    myo.box_factor = 0.45
     myo.rotSetCenter()
     setup()
     myo.unlock("hold")
     
-def onPeriodic():
-    if cntPeriod == 3:
-        if (myo.getHBox() == 1):
-            lowerArm.reverse()
-            lowerArm.connect()
-            armPi.event_trigger(lowerArm.get_interest())
-        elif (myo.getHBox() == 0):
-            lowerArm.set_pwInc(abs(lowerArm.get_pwInc()))
-            lowerArm.disconnect()
-            armPi.event_trigger(lowerArm.get_interest())
-        elif(myo.getHBox() == -1):
-            lowerArm.connect()
-            armPi.event_trigger(lowerArm.get_interest())
-            
+def onBoxChange(boxNumber, state):
+    
+    global armPi
+    
+    
+    
+    if (lowerArm.get_pulse() >= lowerArm.get_maxPulse() - abs(lowerArm.get_pwInc())):
+        lowerArm.disconnect()
+        armPi.event_trigger(lowerArm.get_interest())
+        lowerArm.set_pulse(lowerArm.get_maxPulse() - 2 * abs(lowerArm.get_pwInc()))
+        lowerArm.set_pwInc(abs(lowerArm.get_pwInc()))
         
-        if (myo.getVBox() == 1):
-            upperArm.connect()
-            armPi.event_trigger(upperArm.get_interest())
-        elif (myo.getVBox() == 0):
-            upperArm.set_pwInc(abs(upperArm.get_pwInc()))
-            upperArm.disconnect()
-            armPi.event_trigger(upperArm.get_interest())
-        elif (myo.getVBox() == -1):
-            upperArm.reverse()
-            upperArm.connect()
-            armPi.event_trigger(upperArm.get_interest())
+        armPi.set_servo_pulsewidth(lowerArm.get_gpio(), lowerArm.get_pulse())
+        #time.sleep(0.1)
+    elif (lowerArm.get_pulse() <= lowerArm.get_minPulse() + abs(lowerArm.get_pwInc())):
+        lowerArm.disconnect()
+        armPi.event_trigger(lowerArm.get_interest())
+        lowerArm.set_pulse(lowerArm.get_minPulse() + 2 * abs(lowerArm.get_pwInc()))
+        lowerArm.set_pwInc(abs(lowerArm.get_pwInc()))
+        #add correcting pulses here
+        armPi.set_servo_pulsewidth(lowerArm.get_gpio(), lowerArm.get_pulse())
+        #time.sleep(0.1)
+    elif (myo.getHBox() == 1) and (state == "on"): #((boxNumber == 8) or (boxNumber == 1) or (boxNumber == 2)) and (state == 'on'):
+        lowerArm.reverse()
+        lowerArm.connect()
+        armPi.event_trigger(lowerArm.get_interest())
+    elif (myo.getHBox() == 0) and (state == "on"): #((boxNumber == 7) or (boxNumber == 0) or (boxNumber == 3)) and (state == 'on'):
+        lowerArm.disconnect()
+        lowerArm.set_pwInc(abs(lowerArm.get_pwInc()))
+        armPi.event_trigger(lowerArm.get_interest())
+    elif (myo.getHBox() == -1) and (state == "on"): #((boxNumber == 6) or (boxNumber == 5) or (boxNumber == 4)) and (state == 'on')
+        lowerArm.connect()
+        armPi.event_trigger(lowerArm.get_interest())
+        
+    
+    if (upperArm.get_pulse() >= upperArm.get_maxPulse() - upperArm.get_pwInc()):
+        upperArm.disconnect()
+        armPi.event_trigger(upperArm.get_interest())
+        upperArm.set_pulse(upperArm.get_maxPulse() - 2 * abs(upperArm.get_pwInc()))
+        upperArm.set_pwInc(abs(upperArm.get_pwInc()))
+        #add correcting pulses here
+        armPi.set_servo_pulsewidth(upperArm.get_gpio(), upperArm.get_pulse())
+        #time.sleep(0.1)
+    elif (upperArm.get_pulse() <= upperArm.get_minPulse() + upperArm.get_pwInc()):
+        upperArm.disconnect()
+        armPi.event_trigger(upperArm.get_interest())
+        upperArm.set_pulse(upperArm.get_minPulse() + 2 * abs(upperArm.get_pwInc()))
+        upperArm.set_pwInc(abs(upperArm.get_pwInc()))
+        #add correcting pulses here
+        armPi.set_servo_pulsewidth(upperArm.get_gpio(), upperArm.get_pulse())
+        #time.sleep(0.1)
+    elif (myo.getVBox() == 1) and (state == "on"): #((boxNumber == 2) or (boxNumber == 3) or (boxNumber == 4)) and (state == 'on'):
+        upperArm.connect()
+        armPi.event_trigger(upperArm.get_interest())
+    elif (myo.getVBox() == 0) and (state == "on"): #((boxNumber == 1) or (boxNumber == 0) or (boxNumber == 5)) and (state == 'on')
+        upperArm.set_pwInc(abs(upperArm.get_pwInc()))
+        upperArm.disconnect()
+        armPi.event_trigger(upperArm.get_interest())
+    elif (myo.getVBox() == -1) and (state == "on"): #((boxNumber == 8) or (boxNumber == 7) or (boxNumber == 6)) and (state == 'on'):
+        upperArm.reverse()
+        upperArm.connect()
+        armPi.event_trigger(upperArm.get_interest())
+    
+    #time.sleep(0.03)
+
     
 
