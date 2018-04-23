bcb228747a79a21c110a0f453a58cac16fdccc2d
joffei <jeoffei@gmail.com>
2018-04-23 15:25:33 +0000

    reading EMG data for learning to make custom poses

--- PyoConnect_v2.0/scripts/testDir.py
+++ PyoConnect_v2.0/scripts/testDir.py
@@ -1,6 +1,8 @@
 # Double tap to unlock
 # kept unlocked until lock explicitly called
 
+import time
+
 cnt = 0
 
 def onUnlock():
@@ -8,39 +10,39 @@ def onUnlock():
     myo.box_factor = 0.5
     myo.rotSetCenter()
     
-def onPoseEdge(pose, edge):
-    #print pose + " " + edge
-    if(pose == 'fist') and (edge == 'on'):
-        print "Fist on"
-    elif(pose == 'fist') and (edge == 'off'):
-        print "Fist off"
-    elif(pose == 'waveIn') and (edge == 'on'):
-        print "Wave In on"
-    elif(pose == 'waveIn') and (edge == 'off'):
-        print "Wave In off"
-    elif(pose == 'waveOut') and (edge == 'on'):
-        print "Wave Out on"
-    elif(pose == 'waveOut') and (edge == 'off'):
-        print "Wave Out off"
-    elif(pose == 'fingersSpread') and (edge == 'on'):
-        print "finger Spread on"
-    elif(pose == 'fingersSpread') and (edge == 'off'):
-        print "fingers Spread off"
-    elif(pose == 'rest') and (edge == 'on'):
-        print "rest on"
-    elif(pose == 'rest') and (edge == 'off'):
-        print "rest off"
-    elif(pose == 'doubleTap') and (edge == 'on'):
-        print "double Tap on"
-    elif(pose == 'doubleTap') and (edge == 'off'):
-        print "double Tap off"
-    elif(pose == 'unknown') and (edge == 'on'):
-        print "unknown on"
-    elif(pose == 'unknown') and (edge == 'off'):
-        print "unknown off"
-        
-def onBoxChange(boxNumber, state):
-    print(myo.getHBox(), myo.getVBox(), state)
+#def onPoseEdge(pose, edge):
+#    #print pose + " " + edge
+#    if(pose == 'fist') and (edge == 'on'):
+#        print "Fist on"
+#    elif(pose == 'fist') and (edge == 'off'):
+#        print "Fist off"
+#    elif(pose == 'waveIn') and (edge == 'on'):
+#        print "Wave In on"
+#    elif(pose == 'waveIn') and (edge == 'off'):
+#        print "Wave In off"
+#    elif(pose == 'waveOut') and (edge == 'on'):
+#        print "Wave Out on"
+#    elif(pose == 'waveOut') and (edge == 'off'):
+#        print "Wave Out off"
+#    elif(pose == 'fingersSpread') and (edge == 'on'):
+#        print "finger Spread on"
+#    elif(pose == 'fingersSpread') and (edge == 'off'):
+#        print "fingers Spread off"
+#    elif(pose == 'rest') and (edge == 'on'):
+#        print "rest on"
+#    elif(pose == 'rest') and (edge == 'off'):
+#        print "rest off"
+#    elif(pose == 'doubleTap') and (edge == 'on'):
+#        print "double Tap on"
+#    elif(pose == 'doubleTap') and (edge == 'off'):
+#        print "double Tap off"
+#    elif(pose == 'unknown') and (edge == 'on'):
+#        print "unknown on"
+#    elif(pose == 'unknown') and (edge == 'off'):
+#        print "unknown off"
+#        
+#def onBoxChange(boxNumber, state):
+#    print(myo.getHBox(), myo.getVBox(), state)
 #    if (myo.getHBox() == 1): #((boxNumber == 8) or (boxNumber == 1) or (boxNumber == 2)) and (state == 'on'):
 #        lowerArm.reverse()
 #        lowerArm.connect()
@@ -66,12 +68,15 @@ def onBoxChange(boxNumber, state):
 #        upperArm.connect()
 #        armPi.event_trigger(upperArm.get_interest())
 
-def onPeriodic():
-    global cnt
-    
-    if (cnt >= 100):
-        print(myo.rotRoll(), myo.rotPitch(), myo.rotYaw())
-        cnt = 0
-    else:
-        cnt += 1
+#def onPeriodic():
+#    global cnt
+#    
+#    if (cnt >= 100):
+#        print(myo.rotRoll(), myo.rotPitch(), myo.rotYaw())
+#        cnt = 0
+#    else:
+#        cnt += 1
         
+def onEMG(emg, moving):
+    print(emg, moving)
+    time.sleep(0.25)
